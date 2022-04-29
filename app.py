ddfe0ebd0e8008385ea4835336a4babe9159455a
Kirk Garrison <Kirkgarrison24601@gmail.com>
2022-04-29 13:56:20 -0700

    updated routing protocols

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dogs.db'
 db = SQLAlchemy(app)
 
+
 class Dogs(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
@@ -14,21 +15,18 @@ class Dogs(db.Model):
 
     def __repr__(self):
         return '<Name %r>' % self.id
-
+ 
 @app.route('/', methods=['GET'])
 def home():
     print('/home page')
     title = "Home"
     return render_template("home.html", title=title)
 
-@app.route('/dogs', methods=['POST', 'GET'])
-def dogs():
+@app.route('/dogs', methods=['POST'])
+def add_new_dogs():
     title = "Our dogs"
-    print('/dogs route, adding new dog method')
     if request.method == "POST":
-        dogs_name = request.form['name']
-        dogs_breed = request.form['breed']
-        new_dog = Dogs(name=dogs_name, breed=dogs_breed)
+        new_dog = get_form_data(request.form)    
         try:
             db.session.add(new_dog)
             db.session.commit()
@@ -40,67 +38,53 @@ def dogs():
         dogs = Dogs.query.order_by(Dogs.date_arrived)
         return render_template("dogs.html", title=title, dogs=dogs)
 
-
-@app.route('/dog/<int:id>', methods=['POST', 'GET', 'DELETE'])
-def dog_viewer(id):
-    dog_view = Dogs.query.get_or_404(id)
+@app.route('/dogs', methods=['GET'])
+def get_all_dogs(dog_id):
+    dogs = Dogs.query.get_or_404(dog_id)
     title = "our dogs"
-    print('single dog view page')
-    if request.method == "POST":
-        dog_view_name = request.form['name']
-        dog_view_breed = request.form['breed']
-        updated_dog = Dogs(name=dog_view_name, breed=dog_view_breed)
+    print('single dog view page')    
+    return render_template('dogs.html', title=title, dogs=dogs)
+
+
+
+def get_form_data(form):
+
+    dog_view_name = request.form.get('name', '') # what happens if this is more than 30 characters?
+    dog_view_breed = request.form.get('breed', '')
+
+    return Dogs(name=dog_view_name, breed=dog_view_breed)
+
+
+@app.route('/dog/<int:dog_id>', methods=['DELETE'])
+def delete_dog(dog_id):
+    if request.method == "DELETE":
         try:
-            db.session.add(updated_dog)
+            db.session.delete()
             db.session.commit()
-            print('add and commited, render template /dogs next')
+            return redirect('/dogs')
+        except:
+            print('Error deleting this dog')
+            return redirect('/dogs')
+    delete_response = "{'deleted': true}"
+    return delete_response
+
+
+@app.route('/dog/<int:dog_id>', methods=['GET'])
+def get_dog(dog_id):
+    dog_view = Dogs.query.get_or_404(dog_id)
+    title = "our dogs"    
+    return render_template('dog.html', title=title, dog_view=dog_view)
+
+
+@app.route('/dog/<int:dog_id>', methods=['PUT'])
+def update_dog(get_form_data):
+    title = "our dogs"
+    if request.method == "PUT":
+        try:
+            updated_dog = get_form_data(request.form)
+            db.session.update(updated_dog)
+            db.session.commit()
             return render_template("dogs.html", title=title)
         except:
-            print('Exception error updating single dog view')
-            return "There was a problem updating this dog"
-    elif request.method == "DELETE":
-        try:
-            db.session.delete(dog_view)
-            db.session.commit()
-            print('Delete and commit just executed')
-            dogs = Dogs.query.order_by(Dogs.date_arrived)
-            return render_template("dogs.html", title=title, dogs=dogs)
-        except:
-            print('Exception error deleting single dog view')
-            return "There was a problem removing this dog"
-    else:
-        return render_template('dog.html', dog_view=dog_view)
-
-
-@app.route('/delete/<int:id>', methods=['DELETE', 'GET'])
-def delete(id):
-    dog_found_home = Dogs.query.get_or_404(id)
-    title = "Our dogs"
-    print('Delete route hit')
-    try:
-        db.session.delete(dog_found_home)
-        db.session.commit()
-        print('delete and commit executed')
-        dogs = Dogs.query.order_by(Dogs.date_arrived)
-        return render_template("dogs.html", title=title, dogs=dogs)
-    except:
-        print('Delete exception error hit')
-        return render_template("dogs.html", title=title, dogs=dogs)
-
-@app.route('/update/<int:id>', methods=['POST', 'GET'])
-def update(id):
-    dog_to_update = Dogs.query.get_or_404(id)
-    print('Update route hit!')
-    if request.method == "POST":
-        dog_to_update.name = request.form['name']
-        dog_to_update.breed = request.form['breed']
-        try:
-            db.session.update(dog_to_update)
-            db.session.commit()
-            print('Update and commit executed')
-            return render_template("update.html", dog_to_update=dog_to_update)
-        except:
-            print('Exception error updating dog')
-            return "There was a problem updating this dog"
-    else:
-        return render_template('dogs.html')
+            print('There was a problem updating this dog')
+            return redirect('/dogs')
